7a3f71e4b5b43f81e25420d2c4a2dbc1780e02d6
Big_Bro <anh.bigbrother@gmial.com>
2024-02-19 23:22:34 +0700

    update3

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,27 +6,33 @@ import { useEffect, useState } from "react";
 
 const Body = ({ data }) => {
     const state = useSelector(store => store);
-    const [dataToday, setDataToday] = useState([]);
+    const [dataAtday, setDataAtday] = useState([]);
+    const [day, setDay] = useState();
     const dispatch = useDispatch();
     const handleClick = (payload) => {
         dispatch(action(payload));
     }
     useEffect(() => {
         if (data) {
-            const toDay = new Date();
-            const newToday = data.filter(x => eval('new ' + x[0]).toDateString() === toDay.toDateString());
-            setDataToday(newToday);
+            if (day === undefined) {
+                const toDay = new Date();
+                setDay(toDay.toISOString().slice(0, 10))
+            }
+            const newToday = data.filter(x => eval('new ' + x[0]).toDateString() === new Date(day).toDateString());
+            setDataAtday(newToday);
         }
-    }, [data])
+    }, [data, day])
     return (
         <div className="relative flex-grow bg-slate-300 flex flex-row">
             <div className="relative w-80 h-full border border-white flex flex-col justify-start items-center p-5 ">
+                <div className="p-3 w-52 rounded-xl"></div>
                 <button className="p-3 w-52 rounded-xl" style={state === 'all' ? { backgroundColor: 'white' } : { backgroundColor: 'transparent' }} onClick={() => handleClick('all')}>All</button>
                 <button className="p-3 w-52 rounded-xl" style={state === 'active' ? { backgroundColor: 'white' } : { backgroundColor: 'transparent' }} onClick={() => handleClick('active')}>Active</button>
                 <button className="p-3 w-52 rounded-xl" style={state === 'left' ? { backgroundColor: 'white' } : { backgroundColor: 'transparent' }} onClick={() => handleClick('left')}>Have left</button>
             </div>
-            <div className="relative w-full h-full p-5 border border-white">
-                <Table data={dataToday} />
+            <div className="relative w-full h-full p-3 border border-white flex flex-col gap-3 justify-start items-center">
+                <input type="date" className="p-3 w-52 h-16 rounded-xl" onChange={(e) => setDay(e.target.value)} defaultValue={day} />
+                <Table data={dataAtday} />
             </div>
         </div>
     )
